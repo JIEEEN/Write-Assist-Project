d8f3b819208145ecaefe2a84cbc40cea839acc34
JIEEEN <tjtpp009@korea.ac.kr>
2023-02-06 01:28:42 +0900

    [ADD] signup

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 //const jwt = require('jsonwebtoken');
 const {User} = require('../database/models/index');
 const auth = express.Router();
@@ -20,7 +20,7 @@ auth.
             });
 
             //중복아이디 존재시 -> ajax로 변경해야됨
-            if((id != db_user.id) || (pwd != db_user.pwd)){
+            if((id != db_user.id) || !bcrypt.compare(pwd, db_user.pwd)){
                 res.status(406).send('login failed');
                 return;
             }
@@ -28,15 +28,34 @@ auth.
             //const hash = await bcrypt.hash(pwd, 10); //pasword 해쉬화
 
 
-            //return res.status(200).json({success: 'true'});
-
-
             res.status(200).json({'isLoginPassed':true, 'hasNovelWritten': false})
         }
         catch(e){
             console.error(e);
             return next(e);
-    };
+        };
+    
+});
+
+auth.
+    route('/signup')
+    .post(async(req, res, next) => {
+        const {id, pwd} = req.body;
+
+        const hash_pwd = await bcrypt.hash(pwd, 10);
+
+        try{
+            const new_user = await User.create({
+                id: id,
+                pwd: hash_pwd
+            });
+            
+            res.status(200).json({'isLoginPassed':true})
+        }catch(e){
+            console.error(e);
+            return next(e);
+        }
+        
 });
 
 module.exports = auth;
